6f92be1c470d9ab232aa24e4a997ddee64216f07
GiomarMC <gmunozcu@unsa.edu.pe>
2024-12-16 14:28:35 -0500

    Se cambia url principal a tabla

--- backend/urls.py
+++ backend/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
-    path('tabla', views.procesos_vista, name='tabla_procesos'),
+    path('', views.procesos_vista, name='tabla_procesos'),
 ]
