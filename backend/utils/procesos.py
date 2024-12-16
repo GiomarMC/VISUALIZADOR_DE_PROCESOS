53767a363e3eaae23b9b2e88e0bfaf9ae4d247d5
GiomarMC <gmunozcu@unsa.edu.pe>
2024-12-16 14:31:33 -0500

    Se añade atributos adicionales randomizando los tiempo de llegada y los tiempos de ejecucion

--- backend/utils/procesos.py
+++ backend/utils/procesos.py
@@ -1,18 +1,17 @@
 import psutil
+import random
+import time
 
 def obtener_prioridad_procesos():
     procesos = []
+    tiempo_actual = 0
     for proceso in psutil.process_iter(attrs=['pid', 'name', 'nice', 'status', 'cpu_percent', 'memory_info']):
         try:
-            procesos.append(proceso.info)
+            info = proceso.info
+            tiempo_actual = tiempo_actual + random.randint(1, 4)
+            info['arrival_time'] = tiempo_actual
+            info['time_execution'] = random.randint(1, 10)
+            procesos.append(info)
         except (psutil.NoSuchProcess, psutil.AccessDenied):
             continue
     return procesos
-
-def clasificar_prioridad(nice):
-    if nice < 0:
-        return "Alta"
-    elif nice == 0:
-        return "Normal"
-    else:
-        return "Baja"
