033b9cbba16351a6a39c9f59274a8045bbeb39c9
GiomarMC <gmunozcu@unsa.edu.pe>
2024-12-16 14:32:37 -0500

    Se cambia el view y se pone como principal a tabla

--- backend/views.py
+++ backend/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
-from backend.utils.procesos import obtener_prioridad_procesos, clasificar_prioridad
-
+from backend.utils.procesos import obtener_prioridad_procesos
 # Create your views here.
-def inicio():
-    return HttpResponse("<h1>¡Hola, mundo!</h1>")
-
 def procesos_vista(request):
     procesos = obtener_prioridad_procesos()
-    for proceso in procesos:
-        proceso['prioridad'] = clasificar_prioridad(proceso['nice'])
     return render(request, 'paginas/tabla.html', {'procesos': procesos})
