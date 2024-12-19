41c2b026122abb2fab72e759b8ad22ee2479c181
GiomarMC <gmunozcu@unsa.edu.pe>
2024-12-19 04:11:50 -0500

    Se añaden los path para enlazar las paginas web a la vista

--- backend/urls.py
+++ backend/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.procesos_vista, name='tabla_procesos'),
+    path('visualizacion/', views.visualizacion, name='visualizacion'),
+    path('datos_procesos/', views.obtener_datos_procesos, name='datos_procesos'),
 ]
