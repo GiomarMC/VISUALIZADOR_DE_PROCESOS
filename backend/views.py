b4ec8722ee000fa4600bd0a90f8233a774b0c536
GiomarMC <gmunozcu@unsa.edu.pe>
2024-12-19 04:10:33 -0500

    Se añaden nuevas vistas para el visualizador de procesos

--- backend/views.py
+++ backend/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from backend.utils.procesos import obtener_procesos
+from threading import Thread
 # Create your views here.
+
+procesos = obtener_procesos()
+
 def procesos_vista(request):
-    procesos = obtener_procesos()
+    global procesos
     return render(request, 'paginas/tabla.html', {'procesos': procesos})
+
+def visualizacion(request):
+    return render(request, 'paginas/visualizacion.html')
+
+def obtener_datos_procesos(request):
+    global procesos
+    return JsonResponse({'procesos': procesos}, safe=False)
